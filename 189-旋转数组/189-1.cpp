79eda050e57e135e6b9feb501064a220a5774245
Zu3zz <zthzththth@163.com>
2019-02-02 01:15:03 +0800

    update 189

--- 189-旋转数组/189-1.cpp
+++ 189-旋转数组/189-1.cpp
@@ -1,1 +1,10 @@
-189.cpp
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int s = nums.size();
+        k = k%s;
+        reverse(nums.begin(), nums.begin() + s - k);
+        reverse(nums.begin()+s-k, nums.begin() + s);
+        reverse(nums.begin(), nums.begin() + s);
+    }
+};
