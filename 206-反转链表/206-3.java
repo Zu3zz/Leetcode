562d4de2022846b945bca77b66f7396ff7d1ca6a
Zu3zz <zthzththth@163.com>
2019-11-08 18:47:50 +0800

    206 java finsihed

--- 206-反转链表/206-3.java
+++ 206-反转链表/206-3.java
@@ -8,10 +8,13 @@
  */
 class Solution {
   public ListNode reverseList(ListNode head) {
-    if(head == null || head.next == null) return head;
-    ListNode newHead = reverseList(head.next);
-    head.next.next = head;
-    head.next = null;
-    return newHead;
+      ListNode newHead = null;
+      while(head!=null){
+          ListNode tmp = head.next;
+          head.next = newHead;
+          newHead = head;
+          head = tmp;
+      }
+      return newHead;
   }
 }
