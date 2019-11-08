144bb528ea9fd1b79c947d5e05d026289355c67f
Zu3zz <zthzththth@163.com>
2019-11-08 16:12:03 +0800

    206 java recurv finished

---
+++ 206-反转链表/206-2.java
@@ -0,0 +1,17 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+  public ListNode reverseList(ListNode head) {
+    if(head == null || head.next == null) return head;
+    ListNode newHead = reverseList(head.next);
+    head.next.next = head;
+    head.next = null;
+    return head;
+  }
+}
