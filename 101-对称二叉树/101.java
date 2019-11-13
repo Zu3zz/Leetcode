8c91d4a44517ed554265ef0b9962067916f10274
Zu3zz <zthzththth@163.com>
2019-11-13 22:32:50 +0800

    101 java finished

---
+++ 101-对称二叉树/101.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+  public boolean isSymmetric(TreeNode root) {
+      if (root == null) return true;
+      return isSymmetric(root.left,root.right);
+  }
+  private boolean isSymmetric(TreeNode left, TreeNode right){
+      if(left == null && right == null) return true;
+      if(left == null || right == null) return false;
+      if(left.val != right.val) return false;
+      return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+  }
+}
