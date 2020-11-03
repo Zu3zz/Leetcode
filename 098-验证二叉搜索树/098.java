a7ee23e7966d5422b1c7ed8df285d3ab8980d1d3
Zu3zz <zthzththth@163.com>
2020-11-04 00:07:17 +0800

    feat: update false

--- 098-验证二叉搜索树/098.java
+++ 098-验证二叉搜索树/098.java
@@ -10,7 +10,7 @@
 class Solution {
   public boolean isValidBST(TreeNode root){
     if(root == null) return true;
-    return valid(root, null, null)
+    return valid(root, null, null);
   }
   private boolean valid(TreeNode node, Integer lower, Integer upper){
     if(node == null) return true;
@@ -20,7 +20,7 @@ class Solution {
     if(lower != null && val <= lower) return false;
     if(upper != null && val >= upper) return false;
 
-    if(!valid(node.left, low, val)) return false;
+    if(!valid(node.left, lower, val)) return false;
     if(!valid(node.right, val, upper)) return false;
 
     return true;
