4cc14a01a8652472f4dafff2dbd6f61e3fb1deee
Zu3zz <zthzththth@163.com>
2020-11-03 23:58:36 +0800

    feat: finish 98 with java in recur

---
+++ 098-验证二叉搜索树/098.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+  public boolean isValidBST(TreeNode root){
+    if(root == null) return true;
+    return valid(root, null, null)
+  }
+  private boolean valid(TreeNode node, Integer lower, Integer upper){
+    if(node == null) return true;
+
+    int val = node.val;
+
+    if(lower != null && val <= lower) return false;
+    if(upper != null && val >= upper) return false;
+
+    if(!valid(node.left, low, val)) return false;
+    if(!valid(node.right, val, upper)) return false;
+
+    return true;
+  }
+}
